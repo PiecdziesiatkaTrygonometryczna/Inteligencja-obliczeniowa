ce3184597669ea60f814a4b0fd0164b5d2b7d7a0
Pięćdziesiątka Trygonometryczna <dawidkalinowski243@gmail.com>
2024-03-19 10:17:49 +0100

    lab4

---
+++ lab4/zad2.py
@@ -0,0 +1,87 @@
+import pandas as pd
+df = pd.read_csv("iris.csv")
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder
+from sklearn.neural_network import MLPClassifier
+
+#A
+
+
+(train_set, test_set) = train_test_split(df.values, train_size=0.7, 
+random_state=13)
+
+#B
+
+
+train_labels = train_set[:, -1]
+test_labels = test_set[:, -1]
+
+label_encoder = LabelEncoder()
+train_labels_encoded = label_encoder.fit_transform(train_labels)
+test_labels_encoded = label_encoder.transform(test_labels)
+
+# print(train_labels_encoded)
+#  [1 1 1 1 1 1 2 1 1 1 0 0 0 2 0 2 2 2 0 1 1 2 0 0 2 0 0 1 1 1 0 0 2 0 1 2 1
+#  1 2 2 0 1 2 0 1 0 1 2 0 1 1 2 1 1 0 1 2 0 1 2 2 0 0 0 0 0 1 0 2 0 0 1 0 2
+#  2 0 2 2 1 0 0 1 1 2 2 0 0 0 0 0 1 1 2 1 2 2 0 1 1 1 2 0 2 0 1]
+
+
+# print(test_labels_encoded)
+# [2 0 2 1 2 0 2 2 0 2 0 0 2 0 2 1 1 2 2 1 2 0 1 2 1 0 1 2 0 2 2 2 1 2 1 0 1
+#  2 1 0 2 2 0 2 1]
+
+
+# print(label_encoder.classes_)
+# ['setosa' 'versicolor' 'virginica']
+
+
+
+#D 
+
+
+# model = MLPClassifier(hidden_layer_sizes=(2,), activation='relu', solver='adam', max_iter=5000)
+# model.fit(train_set[:, :-1], train_labels_encoded)
+
+
+# #E
+
+
+# train_accuracy = model.score(train_set[:, :-1], train_labels_encoded)
+# test_accuracy = model.score(test_set[:, :-1], test_labels_encoded)
+
+# print("score on train data:", train_accuracy)
+# print("score on test data:", test_accuracy)
+
+# score on train data: 0.9619047619047619
+# score on test data: 0.9333333333333333
+
+
+#F
+
+# model_3_neurons = MLPClassifier(hidden_layer_sizes=(3,), activation='relu', solver='adam', max_iter=5000)
+# model_3_neurons.fit(train_set[:, :-1], train_labels_encoded)
+
+# train_accuracy_3_neurons = model_3_neurons.score(train_set[:, :-1], train_labels_encoded)
+# test_accuracy_3_neurons = model_3_neurons.score(test_set[:, :-1], test_labels_encoded)
+
+# print("score on train data with 3 neurons:", train_accuracy_3_neurons)
+# print("score on test data with 3 neurons:", test_accuracy_3_neurons)
+
+# score on train data with 3 neurons: 0.9714285714285714
+# score on test data with 3 neurons: 0.9555555555555556
+
+
+#G
+
+model_2_layers = MLPClassifier(hidden_layer_sizes=(3, 3), activation='relu', solver='adam', max_iter=5000)
+model_2_layers.fit(train_set[:, :-1], train_labels_encoded)
+
+train_accuracy_2_layers = model_2_layers.score(train_set[:, :-1], train_labels_encoded)
+test_accuracy_2_layers = model_2_layers.score(test_set[:, :-1], test_labels_encoded)
+
+
+print("score on train data with 2 layers:", train_accuracy_2_layers)
+print("score on test data with 2 layers:", test_accuracy_2_layers)
+
+# score on train data with 2 layers: 0.9714285714285714
+# score on test data with 2 layers: 0.9777777777777777
---
+++ lab4/zad3.py
@@ -0,0 +1,60 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.neural_network import MLPClassifier
+from sklearn.metrics import accuracy_score, confusion_matrix
+
+# A
+df = pd.read_csv("diabetes 1.csv")
+
+train_set, test_set = train_test_split(df, test_size=0.3, random_state=13)
+
+# B
+
+# model = MLPClassifier(hidden_layer_sizes=(6, 3), activation='relu', max_iter=500, random_state=13)
+
+# # C
+
+# model.fit(train_set.drop(columns=['class']), train_set['class'])
+
+# # D
+
+# predictions = model.predict(test_set.drop(columns=['class']))
+
+# accuracy = accuracy_score(test_set['class'], predictions)
+# print("accuracy:", accuracy)
+
+# conf_matrix = confusion_matrix(test_set['class'], predictions)
+# print("confusion matrix:")
+# print(conf_matrix)
+
+# accuracy: 0.70995670995671
+# confusion matrix:
+# [[123  26]
+#  [ 41  41]]
+
+# E
+
+model = MLPClassifier(hidden_layer_sizes=(10, 5), activation='tanh', max_iter=5000, random_state=13)
+model.fit(train_set.drop(columns=['class']), train_set['class'])
+predictions = model.predict(test_set.drop(columns=['class']))
+
+accuracy = accuracy_score(test_set['class'], predictions)
+print("accuracy:", accuracy)
+
+conf_matrix = confusion_matrix(test_set['class'], predictions)
+print("confusion matrix:")
+print(conf_matrix)
+
+# accuracy: 0.7445887445887446
+# confusion matrix:
+# [[135  14]
+#  [ 45  37]]
+
+
+
+# F 
+
+# Gorsze błędy w przypadku diagnozowania chorób to oczywiście FN (Fałszywie negatywne)
+# z racji na to, że lepiej jest zostać zdiagnozowanym jako chorym, przejść inne badania, i potem jednak okazać się
+# zdrowym, niż zostać zdiagnozowanym jako zdrowy, w rzeczywistości będąc chorym i dać chorobie postępować,
+# nie wiedząć o niej
